1cadcc1984c22cf559da2d9ec60d9c407f78d0ac
Egor Batyr <batyr-ev@yandex.ru>
2023-07-18 23:08:39 +0600

    DPH-000 Set up initial sample of the application

---
+++ src/test/java/info/batyrev/solutions/directory/ofpublic/holidays/PublicHolidayLibApplicationTests.java
@@ -0,0 +1,13 @@
+package info.batyrev.solutions.directory.ofpublic.holidays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicHolidayLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
