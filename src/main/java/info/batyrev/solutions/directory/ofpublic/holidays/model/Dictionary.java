e9e54e3d230f43051e1ba3c06929e4b2b65e0781
Egor Batyr <batyr-ev@yandex.ru>
2023-07-19 22:39:57 +0600

    DPH-000 Optimize imports

--- src/main/java/info/batyrev/solutions/directory/ofpublic/holidays/model/Dictionary.java
+++ src/main/java/info/batyrev/solutions/directory/ofpublic/holidays/model/Dictionary.java
@@ -1,8 +1,6 @@
 package info.batyrev.solutions.directory.ofpublic.holidays.model;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 public class Dictionary extends Entity {
