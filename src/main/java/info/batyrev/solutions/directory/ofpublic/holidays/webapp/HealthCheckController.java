24a97f1a25c1960f492c9a4edf39c11a41e1b80e
Egor Batyr <batyr-ev@yandex.ru>
2023-07-30 17:43:56 +0600

    DPH-005 Health Check

--- src/main/java/info/batyrev/solutions/directory/ofpublic/holidays/webapp/HealthCheckController.java
+++ src/main/java/info/batyrev/solutions/directory/ofpublic/holidays/webapp/HealthCheckController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.management.ManagementFactory;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
